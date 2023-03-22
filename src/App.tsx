d0588d2bbdf50e627ab11ecc1595d32136236494
juliossena <juliosouzasena@gmail.com>
2023-03-21 23:56:33 -0300

    feat: create app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Testando</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

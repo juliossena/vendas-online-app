663889af2f0190a044c5cebf1c9f1254a953fff9
juliossena <juliosouzasena@gmail.com>
2023-03-23 22:19:18 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: #ca1212;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Testando</Text>
+      <Text style={styles.container}>Testando</Text>
+      <TextNew>NOVO TESTE</TextNew>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
 
 export default App;

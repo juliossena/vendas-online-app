32f55b057c7de22eb464961ccf3ababd98469610
juliossena <juliosouzasena@gmail.com>
2023-03-26 19:38:40 -0300

    feat: insert semi-bold font

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -29,7 +29,7 @@ const Button = ({ title, type, disabled, loading, margin, onPress, ...props }: B
 
   const renderText = (color: string) => (
     <>
-      <Text type={textTypes.BUTTON_BOLD} color={color}>
+      <Text type={textTypes.BUTTON_SEMI_BOLD} color={color}>
         {title}
       </Text>
       {loading && <ActivityIndicatorButton color={theme.colors.neutralTheme.white} />}
--- src/shared/components/text/Text.tsx
+++ src/shared/components/text/Text.tsx
@@ -15,22 +15,27 @@ const Text = ({ color, type, ...props }: TextProps) => {
       case textTypes.TITLE_BOLD:
       case textTypes.TITLE_LIGHT:
       case textTypes.TITLE_REGULAR:
+      case textTypes.TITLE_SEMI_BOLD:
         return '24px';
       case textTypes.SUB_TITLE_BOLD:
       case textTypes.SUB_TITLE_LIGHT:
       case textTypes.SUB_TITLE_REGULAR:
+      case textTypes.SUB_TITLE_SEMI_BOLD:
         return '20px';
       case textTypes.BUTTON_BOLD:
       case textTypes.BUTTON_LIGHT:
       case textTypes.BUTTON_REGULAR:
+      case textTypes.BUTTON_SEMI_BOLD:
         return '18px';
       case textTypes.PARAGRAPH_SMALL_BOLD:
       case textTypes.PARAGRAPH_SMALL_LIGHT:
       case textTypes.PARAGRAPH_SMALL_REGULAR:
+      case textTypes.PARAGRAPH_SMALL_SEMI_BOLD:
         return '10px';
       case textTypes.PARAGRAPH_BOLD:
       case textTypes.PARAGRAPH_LIGHT:
       case textTypes.PARAGRAPH_REGULAR:
+      case textTypes.PARAGRAPH_SEMI_BOLD:
       default:
         return '14px';
     }
@@ -50,6 +55,12 @@ const Text = ({ color, type, ...props }: TextProps) => {
       case textTypes.PARAGRAPH_SMALL_LIGHT:
       case textTypes.PARAGRAPH_LIGHT:
         return 'Poppins-Light';
+      case textTypes.BUTTON_SEMI_BOLD:
+      case textTypes.TITLE_SEMI_BOLD:
+      case textTypes.SUB_TITLE_SEMI_BOLD:
+      case textTypes.PARAGRAPH_SMALL_SEMI_BOLD:
+      case textTypes.PARAGRAPH_SEMI_BOLD:
+        return 'Poppins-SemiBold';
       case textTypes.BUTTON_REGULAR:
       case textTypes.TITLE_REGULAR:
       case textTypes.SUB_TITLE_REGULAR:

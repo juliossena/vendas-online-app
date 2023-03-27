b46b76740ef9c94efa6871116c429f63b4b7662a
juliossena <juliosouzasena@gmail.com>
2023-03-26 23:09:17 -0300

    feat: insert Icon in project

---
+++ src/shared/components/icon/Icon.tsx
@@ -0,0 +1,5 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+
+import icoMoonConfig from '../../../../selection.json';
+
+export const Icon = createIconSetFromIcoMoon(icoMoonConfig);

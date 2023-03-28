bb90fafc8424a6b0829621f86e0597868e7d7cc3
juliossena <juliosouzasena@gmail.com>
2023-03-27 23:56:19 -0300

    feat: validate email and password in login screen

---
+++ src/modules/login/hooks/useLogin.ts
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native';
+
+export const useLogin = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleOnPress = async () => {
+    setLoading(true);
+    await axios
+      .post('http://192.168.18.152:8080/auth', {
+        email,
+        password,
+      })
+      .catch(() => {
+        setErrorMessage('Usuário ou senha inválidos');
+      });
+    setLoading(false);
+  };
+
+  const handleOnChangeEmail = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    setErrorMessage('');
+    setEmail(event.nativeEvent.text);
+  };
+
+  const handleOnChangePassword = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+    setErrorMessage('');
+    setPassword(event.nativeEvent.text);
+  };
+
+  return {
+    email,
+    password,
+    loading,
+    errorMessage,
+    handleOnPress,
+    handleOnChangeEmail,
+    handleOnChangePassword,
+  };
+};
--- src/modules/login/screens/Login.tsx
+++ src/modules/login/screens/Login.tsx
@@ -3,21 +3,43 @@ import { View } from 'react-native';
 import Button from '../../../shared/components/button/Button';
 import Input from '../../../shared/components/input/Input';
 import { theme } from '../../../shared/themes/theme';
+import { useLogin } from '../hooks/useLogin';
 import { ContainerLogin, Imagelogo } from '../styles/login.style';
 
 const Login = () => {
-  const handleOnPress = async () => {
-    console.log('clicou');
-  };
+  const {
+    email,
+    password,
+    loading,
+    errorMessage,
+    handleOnPress,
+    handleOnChangeEmail,
+    handleOnChangePassword,
+  } = useLogin();
 
   return (
     <View>
       <ContainerLogin>
         <Imagelogo resizeMode="contain" source={require('../../../assets/images/logo.png')} />
-        <Input margin="0px 0px 8px 0px" placeholder="Digite seu email" title="Email:" />
-        <Input secureTextEntry placeholder="Digite sua senha" title="Senha:" />
+        <Input
+          value={email}
+          errorMessage={errorMessage}
+          margin="0px 0px 8px 0px"
+          placeholder="Digite seu email"
+          title="Email:"
+          onChange={handleOnChangeEmail}
+        />
+        <Input
+          errorMessage={errorMessage}
+          value={password}
+          secureTextEntry
+          placeholder="Digite sua senha"
+          title="Senha:"
+          onChange={handleOnChangePassword}
+        />
         <Button
           type={theme.buttons.buttonsTheme.primary}
+          loading={loading}
           margin="16px"
           title="ENTRAR"
           onPress={handleOnPress}

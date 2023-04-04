af739c58160e50f7f1f938dbb0b0251c824ce2ba
juliossena <juliosouzasena@gmail.com>
2023-04-03 23:11:10 -0300

    feat: insert tab navigation

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unstable-nested-components */
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -5,9 +7,12 @@ import CreateUser from './modules/createUser';
 import Home from './modules/home';
 import Login from './modules/login';
 import Splash from './modules/splash';
+import { Icon } from './shared/components/icon/Icon';
 import { MenuUrl } from './shared/enums/MenuUrl.enum';
+import { theme } from './shared/themes/theme';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const Navigation = () => {
   return (
@@ -20,8 +25,34 @@ const Navigation = () => {
           component={CreateUser}
           options={{ title: 'Criar usuário' }}
         />
-        <Stack.Screen name={MenuUrl.HOME} component={Home} options={{ title: 'Home' }} />
       </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            let iconName: string;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'Orders':
+                iconName = 'cart';
+                break;
+              default:
+                iconName = 'profile';
+                break;
+            }
+
+            return <Icon size={16} name={iconName} color={color} />;
+          },
+          tabBarActiveTintColor: theme.colors.mainTheme.primary,
+          tabBarInactiveTintColor: theme.colors.grayTheme.gray80,
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Orders" component={Home} options={{ title: 'Pedidos' }} />
+        <Tab.Screen name="Settings" component={Home} options={{ title: 'Perfil' }} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
